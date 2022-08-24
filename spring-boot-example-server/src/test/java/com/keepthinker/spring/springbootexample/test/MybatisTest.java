e584a4a32fe1d4b911cba44cfaf82a25d5dbbb2a
Shawn Ke <keepthinker@163.com>
2022-08-24 13:27:38 +0800

    mybatis plus test examples

--- spring-boot-example-server/src/test/java/com/keepthinker/spring/springbootexample/test/MybatisTest.java
+++ spring-boot-example-server/src/test/java/com/keepthinker/spring/springbootexample/test/MybatisTest.java
@@ -12,6 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class MybatisTest {
@@ -23,11 +29,31 @@ public class MybatisTest {
     @Test
     public void testSelect(){
 
-        java.util.List<java.util.Map<String, Object>> list = areaMapper.selectMaps(Wrappers.lambdaQuery(Area.class).select(Area::getId).between(Area::getPopulation, 100L, 100000000L));
+        List<Map<String, Object>> list = areaMapper.selectMaps(Wrappers.lambdaQuery(Area.class).select(Area::getId).between(Area::getPopulation, 100L, 100000000L));
         logger.info("result list: {}", JsonUtils.objectToString(list));
+        for (Map<String, Object> elemMap : list) {
+            for (Map.Entry<String, Object> entry : elemMap.entrySet()) {
+                if (entry.getValue() instanceof Integer) {
+                    logger.info("select maps, key:{}, Integer: {}", entry.getKey(), entry.getValue());
+                } else if (entry.getValue() instanceof Long) {
+                    logger.info("select maps, key:{}, Long: {}", entry.getKey(), entry.getValue());
+                } else if (entry.getValue() instanceof String) {
+                    logger.info("select maps, key:{}, String: {}", entry.getKey(), entry.getValue());
+                } else if (entry.getValue() instanceof Float) {
+                    logger.info("select maps, key:{}, Float: {}", entry.getKey(), entry.getValue());
+                } else if (entry.getValue() instanceof BigDecimal) {
+                    logger.info("select maps, key:{}, BigDecimal: {}", entry.getKey(), entry.getValue());
+                } else if (entry.getValue() instanceof Date) {
+                    logger.info("select maps, key:{}, Date: {}", entry.getKey(), entry.getValue());
+                }
+            }
+        }
 
         Long count = areaMapper.selectCount(Wrappers.lambdaQuery(Area.class).between(Area::getPopulation, 100L, 100000000L));
         logger.info("count: {}", count);
+
+        List<Area> areaList = areaMapper.selectBatchIds(Arrays.asList(1, 2, 3, 4, 5));
+        logger.info("selectBatchIds, areaList:{}", JsonUtils.objectToString(areaList));
 
     }
 }
